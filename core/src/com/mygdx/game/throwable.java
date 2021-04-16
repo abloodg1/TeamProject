e8bd618e018191332598d6aef52c2a70a52277b0
Kerry O'Neill <koneill7@binghamton.edu>
2021-04-16 10:22:22 -0400

    draw sprite

--- core/src/com/mygdx/game/throwable.java
+++ core/src/com/mygdx/game/throwable.java
@@ -2,28 +2,32 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class throwable {
     int screenWidth = Gdx.graphics.getWidth();
     int screenHeight = Gdx.graphics.getHeight();
     Texture img;
     double grav = 5;
-    double x = Math.random() * screenWidth;
-    double y = 0;
+    float x = (float)(Math.random() * screenWidth);
+    float y = 0;
     double minHeight;
     double maxHeight = screenHeight - 100;
-    double xV;
-    double yV;
+    float xV;
+    float yV;
 
     public throwable(Texture sprite){
         img = sprite;
-        xV = 1 + (Math.random() * 5);
-        yV = 6 + (Math.random() * 3);
+        xV = (float)(1 + (Math.random() * 5));
+        yV = (float)(6 + (Math.random() * 3));
         if(x > screenWidth/2){
             xV = xV * -1;
         }
     }
 
+    public void draw(SpriteBatch batch){
+        batch.draw(img, x, y, (float)img.getWidth(), (float)img.getHeight());
+    }
     public double getX(){
         return x;
     }
