487e24bd4e806846a6cbd47ea95020984f55f718
XChronosCS <eezor1@binghamton.edu>
2021-04-26 10:58:15 -0400

    Committing changes to group project: 4/26/2021 - submitting changes to SliceInputProcessor

--- core/src/com/mygdx/game/states/Slice/SliceInputProcessor.java
+++ core/src/com/mygdx/game/states/Slice/SliceInputProcessor.java
@@ -1,8 +1,23 @@
 package com.mygdx.game.states.Slice;
 
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.utils.Handler;
+import com.mygdx.game.utils.Utils;
+
+//At least X amount of points for a swipe to be considered.
+//if (Number of overlapping points > X), book gets cut.
 
 public class SliceInputProcessor implements InputProcessor {
+    private final Handler handler;
+    Vector2[] points;
+
+    public SliceInputProcessor() {
+        handler = Handler.getInstance();
+
+    }
+
     @Override
     public boolean keyDown(int keycode) {
         return false;
@@ -20,8 +35,13 @@ public class SliceInputProcessor implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 mathVector = Utils.getCameraPosFromMousePos(handler.camera, screenX, screenY);
+        float translatedX = mathVector.x;
+        float translatedY = mathVector.y;
+        this.points[0] = new Vector2(translatedX, translatedY);
         return false;
     }
+
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
